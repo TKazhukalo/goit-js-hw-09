4efc9f772b62cdd03b15b534fcb6d7253c9ff141
TKazhukalo <118135022+TKazhukalo@users.noreply.github.com>
2023-04-13 21:25:30 +0300

    create-tast-09-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+import '../css/common.css';
+  const bodyEl = document.querySelector('body');
+  const btnStart = document.querySelector('[data-start]');
+  const btnStop = document.querySelector('[data-stop]');
+  btnStart.addEventListener('click', onClickBtnStart);
+  btnStop.addEventListener('click', onClickBtnStop);
+  let timerId = null;
+function onClickBtnStart() {
 
+  timerId = setInterval(randomColor, 1000);
+
+  btnStart.toggleAttribute('disabled');
+  btnStop.removeAttribute('disabled');
+  
+}
+
+function onClickBtnStop() {
+  clearInterval(timerId);
+
+  btnStart.removeAttribute('disabled');
+  btnStop.toggleAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function randomColor() {
+       bodyEl.style.background = getRandomHexColor();
+ }
