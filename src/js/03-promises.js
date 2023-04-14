42f3a59a57cc5d19e3045a043bb1ce2fdf05eb5c
TKazhukalo <118135022+TKazhukalo@users.noreply.github.com>
2023-04-15 00:57:26 +0300

    create-hw-09-task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,8 @@
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import '../css/common.css';
+const btnStart=document.querySelector('button')
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +11,10 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.info(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+   Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
